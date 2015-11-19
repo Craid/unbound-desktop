91288f6659e8f9d1da66f6278735bedb73e2f3d3
Craid <michel.schmitz_1992@hotmail.com>
2015-11-19 01:36:29 +0100

    Initial

---
+++ src/de/thm/sag/unbound/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.thm.sag.unbound.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.thm.sag.unbound.UnboundGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new UnboundGame(), config);
+	}
+}
