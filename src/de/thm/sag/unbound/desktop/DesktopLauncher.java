6b13cd0cd1ea93d7efd7ece25d2c503ff78f5dcf
Craid <michel.schmitz_1992@hotmail.com>
2016-04-08 23:30:37 +0200

    Added Assets Folder as Linked Ressource

--- src/de/thm/sag/unbound/desktop/DesktopLauncher.java
+++ src/de/thm/sag/unbound/desktop/DesktopLauncher.java
@@ -3,11 +3,11 @@ package de.thm.sag.unbound.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
-import de.unbound.UnboundGame;
+import de.unbound.TestGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new UnboundGame(), config);
+		new LwjglApplication(new TestGame(), config);
 	}
 }
